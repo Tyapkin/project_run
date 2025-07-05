73908008c420c3dd4d48a90ad44b24b1ddbe17b7
Alexander Tyapkin <4105222+Tyapkin@users.noreply.github.com>
2025-07-05 19:07:48 +0300

    Add `company_details` API endpoint to provide company metadata

--- project_run/settings/base.py
+++ project_run/settings/base.py
@@ -112,3 +112,7 @@ STATIC_ROOT = 'static'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+COMPANY_NAME = 'Running Skuf'
+SLOGAN = 'Run, Skuf, Run'
+CONTACTS = '@pan_ded'
