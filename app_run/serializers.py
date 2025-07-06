29f79565152740fb7f679a1de897ee9cc2211d74
Alexander Tyapkin <4105222+Tyapkin@users.noreply.github.com>
2025-07-06 23:24:43 +0300

    Add `type` field to `UserSerializer` to differentiate user roles

--- app_run/serializers.py
+++ app_run/serializers.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 
 from app_run.models import Run
 
@@ -12,6 +12,13 @@ class RunSerializer(ModelSerializer):
 
 
 class UserSerializer(ModelSerializer):
+    type = SerializerMethodField()
+
     class Meta:
         model = User
-        fields = ['id', 'username', 'date_joined', 'first_name', 'last_name']
+        fields = ['id', 'username', 'date_joined', 'first_name', 'last_name', 'type']
+
+    def get_type(self, obj):
+        if obj.is_staff:
+            return 'coach'
+        return 'athlete'
